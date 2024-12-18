8f197723e10f6ee972fa975233cb3c643ec8c69d
samandar_shinnazarov <161921451+shinnazarovsamandar@users.noreply.github.com>
2024-12-18 04:26:32 -0500

    46-commit

--- admins/views.py
+++ admins/views.py
@@ -8,28 +8,28 @@ from config.utils import create_response_body
 from .models import GasStationModel
 from .serializers import GasStationGeoFeatureModelSerializer, _GasStationGeoFeatureModelSerializer
 # Create your views here.
-class GasStationCreateAPIView(generics.ListCreateAPIView):
+class GasStationCreateAPIView(generics.CreateAPIView):
     queryset = GasStationModel.objects.all()
     serializer_class = _GasStationGeoFeatureModelSerializer
     # permission_classes = [IsAuthenticated]
 
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_create(serializer)
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(create_response_body("Gas-Station created successfully."), headers=headers)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(create_response_body("Gas-Station created successfully."), headers=headers)
 
-class GasStationListAPIView(generics.RetrieveUpdateDestroyAPIView):
+class GasStationListAPIView(generics.ListAPIView):
     queryset = GasStationModel.objects.all()
     serializer_class = _GasStationGeoFeatureModelSerializer
     # permission_classes = [IsAuthenticated]
 
-    # def list(self, request, *args, **kwargs):
-    #     queryset = self.get_queryset().order_by("-created_at")
-    #     serializer = self.get_serializer(queryset, many=True)
-    #
-    #     data = {
-    #         "gas-stations": serializer.data
-    #     }
-    #     return Response(create_response_body("Gas stations retreived successfully.", data))
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().order_by("-created_at")
+        serializer = self.get_serializer(queryset, many=True)
+
+        data = {
+            "gas-stations": serializer.data
+        }
+        return Response(create_response_body("Gas stations retreived successfully.", data))
